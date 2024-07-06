d898b958a149c9edc3e23a20e910ef2263c8dd6a
ssolpang <iless3377@naver.com>
2024-07-06 19:55:03 +0900

    first async, await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,19 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-	res.render("home",{pageTitle: "Home"});
+const handleSearch = (error, videos) => {
+	console.log("errors", error);
+	console.log("videos", videos);
+};
+
+export const home = async (req, res) => {
+	try {
+		const videos = await Video.find({});
+		return res.render("home", { pageTitle: "Home", videos });
+	} catch (error) {
+		return res.render("server-error", {error});
+	}
 }
+
 export const watch = (req, res) => {
 	const { id } = req.params;
 	return res.render("watch", {pageTitle: `Watching: `});
