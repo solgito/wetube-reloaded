b2bdcf46462b9701e4cced31f7c2bb03317a121e
ssolpang <iless3377@naver.com>
2024-07-14 02:22:05 +0900

    save video into DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,3 +29,24 @@ export const postEdit = (req, res) => {
 	const { title } = req.body;
 	return res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => {
+	return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = async (req, res) => {
+	const { title, description, hashtags } = req.body;
+
+	await Video.create({
+		title,
+		description,
+		createdAt: Date.now(),
+		hashtags: hashtags.split(",").map(word => `#${word}`),
+		meta: {
+			views: 0,
+			rating: 0,
+		},
+	});
+
+	return res.redirect("/");
+};
