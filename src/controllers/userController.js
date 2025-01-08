37346565ecc168350609a9f8d9168eca1476cff7
ssolpang <iless3377@naver.com>
2025-01-08 10:02:51 +0900

    feat/github OAuth 소셜 로그인 구현

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -47,7 +47,7 @@ export const getLogin = (req, res) => {
 export const postLogin = async (req, res) => {
 	const { username, password } = req.body;
 	const pageTitle = "Login";
-	const user = await User.findOne({ username });
+	const user = await User.findOne({ username, socialOnly: false });
 	if (!user) {
 		return res.status(400).render("login", { pageTitle, errorMessage: "An account with this username does not exists." });
 	}
@@ -62,7 +62,93 @@ export const postLogin = async (req, res) => {
 	res.redirect("/");
 };
 
-export const logout = (req, res) => res.send("Logout");
+export const startGithubLogin = (req, res) => {
+	const baseUrl = "https://github.com/login/oauth/authorize";
+	const config = {
+		client_id: process.env.GH_CLIENT,
+		allow_signup: false,
+		scope: "read:user user:email"
+	};
+	const params = new URLSearchParams(config).toString();
+	const finalUrl = `${baseUrl}?${params}`;
+
+	return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = async (req, res) => {
+	const baseUrl = "https://github.com/login/oauth/access_token";
+	const config = {
+		client_id: process.env.GH_CLIENT,
+		client_secret: process.env.GH_SECRET,
+		code: req.query.code,
+	};
+	const params = new URLSearchParams(config).toString();
+	const finalUrl = `${baseUrl}?${params}`;
+	const data = await fetch(finalUrl, {
+		method: "POST",
+		headers: {
+			headers: "application/json",
+		},
+	});
+
+	const text = await data.text();
+	const dataParams = new URLSearchParams(text);
+	const tokenRequest = Object.fromEntries(dataParams.entries());
+
+	if ("access_token" in tokenRequest) {
+		const { access_token } = tokenRequest;
+		const apiUrl = "https://api.github.com";
+
+		const userData = await (
+			await fetch(`${apiUrl}/user`, {
+				headers: {
+					Authorization: `token ${access_token}`,
+				},
+			})
+		).json();
+
+		const emailData = await (
+			await fetch(`${apiUrl}/user/emails`, {
+				headers: {
+					Authorization: `token ${access_token}`,
+				},
+			})
+		).json();
+		
+		const emailObj = emailData.find(
+			(email) => email.primary === true && email.verified === true
+		);
+		if (!emailObj) {
+			return res.redirect("/login");
+		}
+
+		let user = await User.findOne({ email: emailObj.email });
+		if (!user) {
+			user = await User.create({
+				name: userData.name,
+				username: userData.login,
+				email: emailObj.email,
+				avatarUrl: userData.avatar_url,
+				password: "",
+				socialOnly: true,
+				location: userData.location,
+			});
+		} else {
+			req.session.loggedIn = true;
+			req.session.user = user;
+			res.redirect("/");
+		}
+
+	} else {
+		return res.redirect("/login");
+	}
+};
+
+export const logout = (req, res) => {
+	req.session.destroy();
+	return res.redirect("/");
+};
+
 export const see = (req, res) => res.send("See user");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,10 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
 	email: { type: String, required: true, unique: true },
+	socialOnly: { type: Boolean, default: false },
+	avatarUrl: String,
 	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
+	password: { type: String },
 	email: { type: String, required: true },
 	name: { type: String, required: true },
 	location: String,
