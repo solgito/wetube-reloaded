f0775718ce0394c4afe7fa92324ece9e022ba44f
ssolpang <iless3377@naver.com>
2025-01-13 21:06:27 +0900

    feat/video 플레이어의 totalTime 설정

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -38,7 +39,16 @@ const handleMuteClick = (e) => {
 	video.volume = value;
   };
 
+  const handleLoadedMetaData = () => {
+	totalTime.innerText = Math.floor(video.duration);
+  };
+
+  const handleTimeUpdate = () => {
+	currentTime.innerText = Math.floor(video.currentTime);
+  };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetaData);
+video.addEventListener("timeupdate", handleTimeUpdate);
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { logout, see, startGithubLogin, finishGithubLogin, getEdit, postEdit, getChangePassword, postChangePassword } from "../controllers/userController";
-import { avatarUpload, protectorMiddleware } from "../middlewares";
+import { avatarUpload, protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
@@ -9,8 +9,8 @@ userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUpload.single("avatar"), postEdit);
 userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 
 userRouter.get("/:id([0-9a-f]{24})", see);
 
